0bc53b30dede7780a52a7ea5102aed43fc72aff5
Alex <43788117+Developer1414@users.noreply.github.com>
2024-01-05 17:00:42 +0300

    Update random_number.php

--- random_number.php
+++ random_number.php
@@ -4,12 +4,11 @@ if ($_SERVER['REQUEST_METHOD'] === 'GET') {
     // Генерируем рандомное число от 1 до 100
     $randomNumber = rand(1, 100);
 
-    // Возвращаем ответ в формате JSON
-    header('Content-Type: application/json');
-    echo json_encode(['randomNumber' => $randomNumber]);
+    // Возвращаем рандомное число
+    echo $randomNumber;
 } else {
     // Возвращаем ошибку, если запрос не был GET
     header("HTTP/1.1 405 Method Not Allowed");
-    echo json_encode(['error' => 'Method Not Allowed']);
+    echo 'Method Not Allowed';
 }
 ?>
